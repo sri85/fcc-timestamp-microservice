ec1e2cf9b124a55a1846a4ca7c6811f79d1b6fad
sri85 <sripathi@roxtr.com>
2018-03-31 21:02:10 +0100

    made the port to be a env var

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.route('/')
   .get((req, res) => {
     res.sendFile(`${process.cwd()}/views/index.html`);
   });
-
-app.listen(3000, (err) => {
+const port = process.env.PORT || 8080;
+app.listen(port, (err) => {
   if (err) {
     throw err;
   }
